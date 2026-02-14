7103bdefb2997262d7e9dfbd52b9c07a988ea01e
AMEENA NAZRIN <nargeesameena@gmail.com>
2026-02-14 16:09:53 +0530

    added logic section and time complexity

--- algorithms/marriageEstimator.js
+++ algorithms/marriageEstimator.js
@@ -1,4 +1,17 @@
+/* ----LOGIC----
+* Calculate difference in days between the two birthdays
+* Add difference to the day they met
+* Avoid being too close to first fight (d4)
+* If so add golden ratio(in days to the result date)
+* Compute year using average age at meeting + golden ratio
+
+  ----Time Complexity----
+    O(1)(constant time)  
+    no loops over data
+*/
+
 export function generateMDate(date1 = "", date2 = "", date3 = "", date4 = "") {
+
   if (!date1 || !date2 || !date3 || !date4) return null;
 
   const d1 = new Date(date1);
@@ -6,11 +19,11 @@ export function generateMDate(date1 = "", date2 = "", date3 = "", date4 = "") {
   const d3 = new Date(date3);
   const d4 = new Date(date4);
 
-  //difference in days between date2 and date3
-  const diffInDays = Math.ceil(Math.abs(d3 - d2) / (1000 * 60 * 60 * 24));
+  //difference in days between date2 and date1
+  const diffInDays = Math.ceil(Math.abs(d1 - d2) / (1000 * 60 * 60 * 24));
 
-  //add difference to date1
-  let resultDate = new Date(d1);
+  //add difference to date3
+  let resultDate = new Date(d3);
   resultDate.setDate(resultDate.getDate() + diffInDays);
 
   //30 days gap from date4
@@ -28,6 +41,6 @@ export function generateMDate(date1 = "", date2 = "", date3 = "", date4 = "") {
   const resultYear = d3.getFullYear() + goldenOffset;
 
   resultDate.setFullYear(resultYear);
-  
+
   return resultDate.toISOString().split("T")[0];
 }
